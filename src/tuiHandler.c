a4c834998b1c157dc59ed3dc6f02eb276dd761e6
Denis310599 <denis31599@hotmail.com>
2025-02-11 21:45:01 +0100

    Relative positioning looking good

--- src/tuiHandler.c
+++ src/tuiHandler.c
@@ -1,7 +1,9 @@
 #include <fcntl.h>
 #include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <termios.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
@@ -9,6 +11,12 @@
 #include "../include/renderer.h"
 #include "../include/viewport.h"
 
+/*Appearance Structures*/
+typedef struct Color{
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
+} Color;
 
 /*Structures related with components*/
 
@@ -40,6 +48,10 @@ typedef struct Component{
   int width;
 	int real_height;
 	int real_width;
+	int maxHeight;
+	int minHeight;
+	int maxWidth;
+	int minWidth;
   int isUpdated;
 
 	//Dynamic positioning
@@ -56,10 +68,26 @@ typedef struct Component{
 	int marginStart;
 	int marginEnd;
 	int margin;
-	
+	int paddingTop;
+	int paddingBot;
+	int paddingStart;
+	int paddingEnd;
+	int padding;
+	float xBias;
+	float yBias;
+
 	//Dynamic sizing
-	int autoHeight; //0 noAuto, 1 fitContent, 2 fitSpace
+	int autoHeight; //0 noAuto, 1 fitContent, 2 fitSpace, 3 percentaje
+	float heightBias;
 	int autoWidth;
+	float widthBias;
+
+	//Appearance
+	int border; //0 no border, 1 normal border, 2 big border
+	Color backgroundColor;
+	Color borderColor;
+
+
 
 	//Custom settings
   union{
@@ -74,7 +102,7 @@ void initUI();
 void drawUI();
 void drawContainer(Component * component, Component * parent);
 void drawComponent(Component * component, Component * parent);
-void drawBox(int x, int y, int height, int width);
+void drawBox(int x, int y, int height, int width, int drawBorder, Color bgColor, Color borderColor);
 void drawViewport();
 void mark_component_as_updated(Component * component);
 void handle_resize();
@@ -82,6 +110,7 @@ void handle_resize();
 void calculateComponentDimensions(Component * component, Component * parent, int mode);
 
 Component * newContainer();
+Color * newColor(uint8_t r, uint8_t g, uint8_t b);
 
 int handleInput();
 void closeProgram();
@@ -96,6 +125,10 @@ void get_terminal_size(int *rows, int *cols);
 /*File accessible variables*/
 Component parentComponent;
 
+/*Color deffinitions*/
+const Color BG_COLOR = (Color){7, 18, 36};
+const Color FONT_COLOR= (Color){173, 173, 173};
+const Color BG_2_COLOR= (Color){29, 42, 64};
 int main(){
 	DEBUG=1;
   initUI();
@@ -126,8 +159,8 @@ int main(){
 
     drawUI();
 		//getchar();
- //int aux = 0/0;
-		vp_render_viewport(&viewport);
+    //int aux = 0/0;
+		//vp_render_viewport(&viewport);
     
   }
 }
@@ -136,18 +169,22 @@ void initUI(){
   //Prepares the terminal
   prepareTerminal();
 
+	parentComponent = *newContainer();
   //Calculates component table
   parentComponent.x = 0;
-  parentComponent.y = 1;
+  parentComponent.y = 0;
 
   int rows, cols;
   get_terminal_size(&rows, &cols);
   parentComponent.height = rows;
   parentComponent.width = cols-1;
   parentComponent.real_width= cols-1;
-  parentComponent.isUpdated = 1;
+  parentComponent.isUpdated = 3;
   parentComponent.childCount = 4;
 	parentComponent.component_type = container_t;
+	parentComponent.backgroundColor = BG_COLOR;
+	parentComponent.border = 1;
+	parentComponent.padding = 0;
 
 	parentComponent.children = malloc(2*sizeof(Component *));
 	parentComponent.children[0] = newContainer();
@@ -179,12 +216,16 @@ void initUI(){
   Component * contChild = newContainer();
 	contChild->childCount = 1;
 	contChild->x = 50;
-	contChild->autoHeight = 1;
+	contChild->autoHeight = 2;
 	contChild->height = 10;
 	contChild->real_width = 10;
+	contChild->backgroundColor = BG_2_COLOR;
+	contChild->border = 0;
 	//contChild->marginTop = 5;
-	contChild->topToTopOf = parentComponent.children[0];
-	contChild->bottomToBottomOf = parentComponent.children[2];
+	contChild->topToTopOf = &parentComponent;//parentComponent.children[0];
+	contChild->bottomToBottomOf = &parentComponent;// parentComponent.children[2];
+	contChild->yBias = 0.3f;
+	contChild->heightBias = 0.5f;
 	parentComponent.children[3] = contChild;
 
 	contChild->children = malloc(1*sizeof(Component *));
@@ -221,6 +262,8 @@ void prepareTerminal(){
   //Swaps buffer
   printf("\033[?1049h");
   fflush(stdout);
+	printf("\033[?25l");
+  fflush(stdout);
 
   //Enable raw input mode
   enable_raw_mode();
@@ -250,7 +293,7 @@ void handle_resize(){
 
 void mark_component_as_updated(Component * component){
 	component->real_height = -1;
-	component->isUpdated = 1;
+	component->isUpdated = 3;
 
 	for(int i = 0; i<component->childCount; i++){
 		mark_component_as_updated(component->children[i]);
@@ -263,6 +306,8 @@ void restoreTerminal(){
   
   //Restores buffer
   printf("\033[?1049l");
+  fflush(stdout);
+  printf("\033[?25h");
   fflush(stdout);
 
 }
@@ -318,19 +363,25 @@ void drawComponent(Component * component, Component * parent){
 void drawContainer(Component * component, Component * parent){
 	debug("Drawing container %d.\n X: %d, Y: %d,\n height: %d,\n width: %d", component, component->x, component->y, component->real_height, component->real_width);
   if(component->isUpdated== 0) return;
-  component->isUpdated = 0;
+  component->isUpdated--;
 
   //Draws the border
   if(component == &parentComponent) {
 		drawBox(component->x,
           component->y,
           component->real_height,
-          component->real_width);
+          component->real_width,
+					component->border,
+					component->backgroundColor,
+					(Color){});
 	}else if(parent != NULL){
 		drawBox(component->x + parent->x,
           component->y+ parent->y,
           component->real_height,
-          component->real_width);
+          component->real_width,
+					component->border,
+					component->backgroundColor,
+					(Color){});
 
 	}
   
@@ -340,7 +391,7 @@ void drawContainer(Component * component, Component * parent){
 	}
 }
 
-void drawBox(int x, int y, int height, int width){
+void drawBox(int x, int y, int height, int width, int drawBorder, Color bgColor, Color borderColor){
   int true_x = x;
   int true_y = y+1;
   int true_end_x = x+width;
@@ -361,21 +412,33 @@ void drawBox(int x, int y, int height, int width){
   for (int y_iter = true_y; y_iter <=true_end_y; y_iter++){
     for(int  x_iter = true_x; x_iter <= true_end_x; x_iter++){
       //if(y_iter == true_y) printf("%s", ".");
-      if(x_iter == true_x && y_iter == true_y){ 
-        printf("%s", "╭");
-      }else if(x_iter == true_x && y_iter == true_end_y){ 
-        printf("%s", "╰");
-      }else if(x_iter == true_end_x && y_iter == true_y){
-        printf("%s", "╮");
-      }else if(x_iter == true_end_x && y_iter == true_end_y){
-        printf("%s", "╯");
-      }else if(y_iter == true_y || y_iter == true_end_y){ 
-        printf("%s", "─");
-      }else if(x_iter == true_x || x_iter == true_end_x){
-        printf("%s", "│");
-      }else{
-        printf(" ");  
-      }
+			if(drawBorder == 1){
+				if(x_iter == true_x && y_iter == true_y){ 
+					printf("\e[48;2;%d;%d;%dm%s", bgColor.r, bgColor.g, bgColor.b, "╭");
+					fflush(stdout);
+				}else if(x_iter == true_x && y_iter == true_end_y){ 
+					printf("\e[48;2;%d;%d;%dm%s", bgColor.r, bgColor.g, bgColor.b, "╰");
+					fflush(stdout);
+				}else if(x_iter == true_end_x && y_iter == true_y){
+					printf("\e[48;2;%d;%d;%dm%s", bgColor.r, bgColor.g, bgColor.b, "╮");
+					fflush(stdout);
+				}else if(x_iter == true_end_x && y_iter == true_end_y){
+					printf("\e[48;2;%d;%d;%dm%s", bgColor.r, bgColor.g, bgColor.b, "╯");
+					fflush(stdout);
+				}else if(y_iter == true_y || y_iter == true_end_y){ 
+					printf("\e[48;2;%d;%d;%dm%s", bgColor.r, bgColor.g, bgColor.b, "─");
+					fflush(stdout);
+				}else if(x_iter == true_x || x_iter == true_end_x){
+					printf("\e[48;2;%d;%d;%dm%s", bgColor.r, bgColor.g, bgColor.b, "│");
+					fflush(stdout);
+				}else{
+					printf("\e[48;2;%d;%d;%dm%s", bgColor.r, bgColor.g, bgColor.b, " ");
+					fflush(stdout);
+				}
+			}else{
+					printf("\e[48;2;%d;%d;%dm%s", bgColor.r, bgColor.g, bgColor.b, " ");
+					fflush(stdout);
+			}
     }
     printf("\033[%d;%dH", y_iter+1, true_x);
     fflush(stdout);
@@ -395,6 +458,8 @@ void calculateComponentDimensions(Component * component, Component * parent, int
 	//Calculates the dimensions
 	int componentMarginTop = component->marginTop == -1 ? component->margin : component->marginTop;
 	int componentMarginBot = component->marginBot == -1 ? component->margin : component->marginBot;
+	int componentPaddingTop = parent->paddingTop== -1 ? parent->padding: parent->paddingTop;
+	int componentPaddingBot = parent->paddingBot== -1 ? parent->padding: parent->paddingBot;
 	int min_y = -1;
 	int max_y = -1;
 
@@ -414,6 +479,7 @@ void calculateComponentDimensions(Component * component, Component * parent, int
 				auxMargin += child->marginBot == -1 ? child->margin : child->marginBot;	
 				auxHeight = child->y + child->real_height + auxMargin;
 				maxHeight = maxHeight<auxHeight ? auxHeight : maxHeight;
+				maxHeight += componentPaddingBot;
 			}
 			component->real_height = maxHeight;
 		debug("  Height: %d", maxHeight);
@@ -440,7 +506,8 @@ void calculateComponentDimensions(Component * component, Component * parent, int
 		min_y += componentMarginTop;
 	}else if(component->bottomToBottomOf == NULL && component->bottomToTopOf == NULL || component->topToTopOf == parent){
 		//Default: topToTopOf: Parent
-		min_y = componentMarginTop;
+		int parentPadding = parent->paddingTop != -1 ? parent->paddingTop : parent->padding;
+		min_y = componentMarginTop + parentPadding;
 	}
 	debug("min_y = %d", min_y);
 
@@ -469,6 +536,10 @@ void calculateComponentDimensions(Component * component, Component * parent, int
 			auxHeight = component->bottomToBottomOf->y + component->bottomToBottomOf->real_height;
 		}
 		auxHeight -= componentMarginBot;
+		if(component->bottomToBottomOf == parent){
+			int paddingParent = parent->paddingBot == -1 ? parent->padding : parent->paddingBot;
+			auxHeight -= paddingParent;
+		}
 		max_y= auxHeight < 0 ? 0 : auxHeight;
 	}else if(min_y != -1){
 		//max_y = component->real_height + min_y;
@@ -493,15 +564,27 @@ void calculateComponentDimensions(Component * component, Component * parent, int
 			max_y = min_y + component->real_height;
 		}
 
-		//Both are deffined, we get the middle point, counting the content block as the content + margins top and bot
-		component->y = min_y + (max_y-min_y-component->real_height)/2;
+		//Both are deffined, we get the middle point (or biased)
+		float auxBias = component->yBias != -1 ? component->yBias*2 : 1;
+		if (component != &parentComponent) component->y = min_y + auxBias * (max_y-min_y-component->real_height)/2;
 
-	}else{
+	}else if(component->autoHeight == 2){
 		//We set the top at the maximun, and also the max size
 		component->y = min_y;
 		//If some of them is not deffined, we assume it 0
 		if(max_y == -1 || min_y == -1) component->real_height = 0;
 		else component->real_height = max_y - min_y;
+
+		//Adjust size according to bias
+		
+		component->real_height *= component->heightBias != -1 ? component->heightBias : 1; 
+		
+		//Both are deffined, we get the middle point (or biased)
+		float auxBias = component->yBias != -1 ? component->yBias*2 : 1;
+		if (component != &parentComponent) component->y = min_y + auxBias * (max_y-min_y-component->real_height)/2;
+		//if (component != &parentComponent) component->y = middlePoint + component->real_height/2;
+
+
 	}
 	debug("Final height component %d: %d", parent, component->real_height);
 	debug("Final y component %d: %d", parent, component->y);
@@ -526,11 +609,26 @@ Component * newContainer(){
 	cont->margin = 0;
 	cont->marginBot = -1;
 	cont->marginTop = -1;
+	cont->paddingTop = -1;
+	cont->paddingBot = -1;
+	cont->padding = 0;
 	cont->component_type = container_t;
 	cont->childCount = 0;
 	cont->real_height = -1;
 	cont->real_width = -1;
-	cont->isUpdated = 1;
+	cont->isUpdated = 3;
+	cont->backgroundColor = BG_COLOR;
+	cont->border = 1;
+	cont->xBias = 1;
+	cont->yBias = 1;
+	cont->heightBias = 1;
+	cont->widthBias = 1;
 
 	return cont;
+}
+
+/*Creates new color based on an Hex string*/
+Color * newColor(uint8_t r, uint8_t g, uint8_t b){
+	Color * retPtr = malloc(sizeof(Color));
+	return retPtr;
 }
